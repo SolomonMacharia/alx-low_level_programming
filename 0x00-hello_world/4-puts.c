ef661edff7e2e718ffb98ad7262d068f5c183b37
Solomon Macharia <solomonmacharia7@gmail.com>
2022-09-11 14:45:52 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
