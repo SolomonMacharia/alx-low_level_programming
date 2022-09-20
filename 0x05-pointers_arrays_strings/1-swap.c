06abc6e51009099d35feecff7d0a942bedcd70cb
Solomon Macharia <solomonmacharia7@gmail.com>
2022-09-20 10:53:01 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @n: input
- * Return: n
- */
+* swap_int - a function that swaps the values of two integers
+* @a: input 1
+* @b: input 2
+* Return: integers
+*/
 
 void swap_int(int *a, int *b)
 {
